989efb810851a6a30f518b78f6c5746fb08e7068
Yasuaki Honda <yasuaki.honda@gmail.com>
2025-08-20 00:51:35 +0900

    Definition of ShannonSecrecy is added.

---
+++ PerfectSecrecy/ShannonSecrecy_Def.lean
@@ -0,0 +1,35 @@
+import PerfectSecrecy.PerfectSecrecy_Def
+open PMF
+
+section ShannonSecrecyDefinition
+
+variable {M K C : Type}
+variable [Fintype M] [Inhabited M]
+variable [Fintype K] [Inhabited K]
+variable [Fintype C] [Inhabited C]
+
+
+noncomputable
+def joint_dist (Enc : K → M → C) (Gen : PMF K) (Msg : PMF M) : PMF (M × C) :=
+  do
+    let m ← Msg
+    let k ← Gen
+    PMF.pure (m, Enc k m)
+
+noncomputable
+def cipher_dist (Enc : K → M → C) (Gen : PMF K) (Msg : PMF M) : PMF C := do
+  let m ← Msg
+  Enc_dist Enc Gen m
+
+noncomputable
+def posterior (Enc : K → M → C) (Gen : PMF K) (Msg : PMF M) (m : M) (c : C) : ENNReal :=
+  let joint := joint_dist Enc Gen Msg
+  if (cipher_dist Enc Gen Msg) c ≠ 0 then
+    (joint (m , c)) / ((cipher_dist Enc Gen Msg) c)
+  else 0
+
+noncomputable
+def shannon_perfect_secrecy (Enc : K → M → C) (Gen : PMF K) : Prop :=
+  ∀ m c, ∀ Msg : PMF M, posterior Enc Gen Msg m c = Msg m
+
+end ShannonSecrecyDefinition
